757b7bcc4852f9e920aef02466743d868f234936
KyuSang Cho <ksang0324@gmail.com>
2023-03-30 16:56:00 +0900

    pyside6_study

---
+++ python_pyside6/tutorial/03signals_and_slots.py
@@ -0,0 +1,45 @@
+# import sys
+# from PySide6.QtWidgets import QApplication, QPushButton
+#
+#
+# def function():
+#     print("The 'function' has been called!")
+#
+#
+# app = QApplication()
+# button = QPushButton("Call function")
+# button.clicked.connect(function)
+# button.show()
+# sys.exit(app.exec())
+
+# -------------------------------------------------------------------------
+
+import sys
+from PySide6.QtCore import QObject, Signal, Slot
+from PySide6.QtWidgets import QApplication, QPushButton
+
+
+class Communicate(QObject):
+    speak = Signal((int,), (str,))
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.speak[int].connect(self.say_something)
+        self.speak[str].connect(self.say_something)
+
+
+    @Slot(int)
+    @Slot(str)
+    def say_something(self, arg):
+        if isinstance(arg, int):
+            print("This is a number:", arg)
+        elif isinstance(arg, str):
+            print("This is a string:", arg)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    someone = Communicate()
+    someone.speak.emit(10)
+    someone.speak[str].emit("Hello everybody!")
---
+++ python_pyside6/tutorial/05using_ui.py
@@ -0,0 +1,24 @@
+# designer through pyside6 designer
+# pyside6-uic mainwindow.ui > ui_mainwindow.py
+
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile
+# from ui_mainwindow import Ui_MainWindow
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
